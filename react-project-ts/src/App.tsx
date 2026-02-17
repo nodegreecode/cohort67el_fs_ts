d6fec7b1edf91a192110908767279549921f7a40
nodegreecode <vasylenkoanatolii1989@gmail.com>
2026-02-17 20:30:45 +0100

    add new homework component

--- react-project-ts/src/App.tsx
+++ react-project-ts/src/App.tsx
@@ -1,9 +1,14 @@
-import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
+//import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
+import Homework_06 from './homeworks/Homework_06/Homework_06.tsx';
 
 function App() {
   return (
     <>
-      <Lesson_06 />
+      {/*LESSONS*/}
+      {/*<Lesson_06 />*/}
+
+      {/*HOMEWORKS*/}
+      <Homework_06 />
     </>
   );
 }
---
+++ react-project-ts/src/homeworks/Homework_06/Homework_06.tsx
@@ -0,0 +1,42 @@
+import "./styles.css";
+
+interface Car {
+  brand: string;
+  price: number;
+  isDiesel: boolean;
+}
+
+const cars: Car[] = [
+  { brand: "BMW", price: 20000, isDiesel: true },
+  { brand: "Mercedes", price: 22000, isDiesel: false },
+  { brand: "Porshe", price: 50000, isDiesel: true },
+  { brand: "Nissan", price: 25000, isDiesel: false },
+  { brand: "Audi", price: 50000, isDiesel: true },
+];
+
+function Homework_06() {
+
+  const carsDetails = cars.map((car: Car) => (
+    <div className="card">
+      <h2 className="card-title"><span>Brand:</span> {car.brand}</h2>
+      <p className="card-text">
+        <span>Price: </span> {car.price}
+      </p>
+      <p className="card-text">
+        <span>Fuel type:</span> {car.isDiesel ? "Diesel" : "Patrol"}
+      </p>
+    </div>
+  ))
+
+  return (
+    <section className="homework_06-section">
+      <div className="container">
+        <div className="card-wrapper">
+          {carsDetails}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Homework_06;
