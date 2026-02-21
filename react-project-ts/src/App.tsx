8449f39c7781ee076bc081263ee934c6a135edde
nodegreecode <vasylenkoanatolii1989@gmail.com>
2026-02-21 14:55:26 +0100

    add lesson 07

--- react-project-ts/src/App.tsx
+++ react-project-ts/src/App.tsx
@@ -1,14 +1,16 @@
 //import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
-import Homework_06 from './homeworks/Homework_06/Homework_06.tsx';
+import Lesson_07 from "./lessons/Lesson_07/Lesson_07";
+//import Homework_06 from "./homeworks/Homework_06/Homework_06.tsx";
 
 function App() {
   return (
     <>
       {/*LESSONS*/}
       {/*<Lesson_06 />*/}
+      <Lesson_07 />
 
       {/*HOMEWORKS*/}
-      <Homework_06 />
+      {/*<Homework_06 />*/}
     </>
   );
 }
