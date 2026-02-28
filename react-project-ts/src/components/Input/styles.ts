09e489ca4387725e199a2c28ede4cb3d3df971da
nodegreecode <vasylenkoanatolii1989@gmail.com>
2026-02-28 12:00:16 +0100

    add lesson-09

--- react-project-ts/src/components/Input/styles.ts
+++ react-project-ts/src/components/Input/styles.ts
@@ -10,7 +10,7 @@ const generateInputColor = (disabled: boolean | undefined): string => {
 };
 
 const generateInputBorder = (error: string | undefined) => {
-  return error && error.length > 0 ? "red" : "";
+  return error && error.length > 0 ? "red" : "#3f3f3f";
 };
 
 export const InputWrapper = styled.div`
@@ -27,11 +27,11 @@ export const InputLabel = styled.label`
 export const InputComponent = styled.input<InputComponentProps>`
   width: 100%;
   height: 50px;
-  border: 1px solid #3f3f3f;
+  border: 1px solid ${({$error})=> generateInputBorder($error)};
   border-radius: 4px;
   padding: 12px;
   outline: none;
-
+  cursor: ${({disabled})=> disabled ? "not-allowed" : "pointer"};
   &::placeholder {
     color: #6f6f6f;
     font-size: 16px;
@@ -39,5 +39,10 @@ export const InputComponent = styled.input<InputComponentProps>`
 
   background-color: ${({ disabled }) => generateInputColor(disabled)};
   
-  border-color: ${({$error})=> generateInputBorder($error)};
+ 
+`;
+
+export const ErrorMessage = styled.p`
+height: 20px;
+  color: #ff6868ff;
 `;
---
+++ react-project-ts/src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,64 @@
+import { type ChangeEvent, use } from "react";
+import { useState } from "react";
+import Button from "../Button/Button";
+import Input from "../Input/Input";
+import { LoginFormContainer, Title, InputsContainer } from "./styles.ts";
+
+function LoginForm() {
+  //const [inputValue, setInputValue] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const changeEmail = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const changePassword = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const login = (event: SubmitEvent) => {
+    event.preventDefault();
+    console.info("Email ", email);
+    console.info("Password ", password);
+  };
+
+  /*const onChangeInputValue = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };*/
+
+  return (
+    <LoginFormContainer onSubmit={login}>
+      <Title>Login form</Title>
+      <InputsContainer>
+        <Input
+          id="email-id"
+          name="email"
+          type="email"
+          placeholder="Enter your email"
+          label="Email"
+          value={email}
+          onChange={changeEmail}
+        />
+        <Input
+          id="password-id"
+          name="password"
+          type="password"
+          placeholder="Enter your password"
+          label="Password"
+          value={password}
+          onChange={changePassword}
+        />
+        {/*        <input
+          onChange={onChangeInputValue}
+          value={inputValue}
+          type="text"
+          placeholder="Enter your text"
+        />*/}
+      </InputsContainer>
+      <Button name="Login" type="submit" onClick={() => console.log("Login")} />
+    </LoginFormContainer>
+  );
+}
+
+export default LoginForm;
