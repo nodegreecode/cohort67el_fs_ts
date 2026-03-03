daa45ea66656537143d0ce872cf04fd05692a285
nodegreecode <vasylenkoanatolii1989@gmail.com>
2026-03-03 18:29:50 +0100

    add new component lesson_10

--- react-project-ts/src/App.tsx
+++ react-project-ts/src/App.tsx
@@ -1,11 +1,12 @@
 //import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
 //import Lesson_07 from "./lessons/Lesson_07/Lesson_07";
 //import Lesson_08 from "./lessons/Lesson_08/Lesson_08";
-//import Lesson_09 from "lessons/Lesson_09/Lesson_09.tsx";
+import Lesson_10 from "lessons/Lesson_10/Lesson_10.tsx";
+
 //import Homework_06 from "./homeworks/Homework_06/Homework_06.tsx";
 //import Homework_07 from "./homeworks/Homework_07/Homework_07.tsx";
 //import Homework_08 from "./homeworks/Homework_08/Homework_08.tsx";
-import Homework_09 from "./homeworks/Homework_09/Homework_09.tsx";
+//import Homework_09 from "./homeworks/Homework_09/Homework_09.tsx";
 import GlobalStyles from "./styles/GlobalStyles.tsx";
 
 function App() {
@@ -16,13 +17,13 @@ function App() {
       {/*<Lesson_06 />*/}
       {/* <Lesson_07 />*/}
       {/*<Lesson_08 />*/}
-        {/*<Lesson_09 />*/}
+        <Lesson_10 />
 
       {/*HOMEWORKS*/}
       {/* <Homework_06 />*/}
       {/* <Homework_07 />*/}
       {/* <Homework_08 />*/}
-      <Homework_09 />
+      {/*<Homework_09 />*/}
     </>
   );
 }
