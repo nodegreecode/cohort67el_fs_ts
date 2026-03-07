7e9cd307d9b08ef26fa00320451d3886288cb282
nodegreecode <vasylenkoanatolii1989@gmail.com>
2026-03-07 14:49:25 +0100

    add project universities

--- react-project-ts/src/components/Button/types.ts
+++ react-project-ts/src/components/Button/types.ts
@@ -1,7 +1,7 @@
 export interface ButtonProps {
   name?: string;
   type?: "button" | "submit" | "reset" | undefined;
-  onClick: () => void;
+  onClick?: () => void;
   isDelete?: boolean;
   disabled?: boolean;
 }
